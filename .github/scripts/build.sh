1df5ceccb0a42d735bf73987a385cf837df0f867
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-03-16 11:08:31 +0100

    Fixing ci

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-cd ../../Quiz
+cd ..
 pwd
+ls -a
+cd ..
+pwd
+ls -a
+cd ..
+pwd
+ls -a
 xcodebuild test -enableCodeCoverage YES -scheme Quiz -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' ONLY_ACTIVE_ARCH=NO
