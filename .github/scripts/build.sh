dc9a1f6bacdefc701acb6d7f99ee6d106fad4f5c
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-03-16 11:56:14 +0100

    Improved readability

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd Quiz
-xcodebuild test -enableCodeCoverage YES -scheme Quiz -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' ONLY_ACTIVE_ARCH=NO
+xcodebuild test -enableCodeCoverage YES -scheme Quiz -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 11' ONLY_ACTIVE_ARCH=NO
